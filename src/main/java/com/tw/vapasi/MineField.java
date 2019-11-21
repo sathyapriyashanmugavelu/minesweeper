9eb983b9ed39b89d3d856fd2baaf96ba5241523c
sathyapriya <spriya2306@gmail.com>
2019-11-21 08:06:40 +0530

    [Sathya Priya]Add.Set flag

--- src/main/java/com/tw/vapasi/MineField.java
+++ src/main/java/com/tw/vapasi/MineField.java
@@ -1,6 +1,37 @@
 package com.tw.vapasi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MineField {
-    public MineField() {
+    private List<Grid> grid;
+    private int length;
+
+    public MineField(String inputString) {
+        grid = new ArrayList<Grid>();
+        final String[] input = inputString.split(",");
+        length = input.length;
+        for (int i = 0; i < length; i++) {
+            String stringInput = input[i];
+            for (int j = 0; j < length; j++) {
+                grid.add(new Grid(i,j,stringInput.charAt(j)));
+            }
+        }
     }
+
+    public void flagCell(int row, int col,char action){
+        Grid openedCell = grid.get(row * length + col);
+        openedCell.setDisplayChar(action);
+    }
+
+//    public void displayMatrix() {
+//        for (int i = 0; i < length; i++) {
+//            for (int j = 0; j < length; j++) {
+//                System.out.print(grid.get(i) + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+
+
 }
--- src/test/java/com/tw/vapasi/MineFieldTest.java
+++ src/test/java/com/tw/vapasi/MineFieldTest.java
@@ -2,9 +2,15 @@ package com.tw.vapasi;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MineFieldTest {
     @Test
-    public void shouldSetIsMineTrueWhenInputStringM(){
-        MineField minefield=new MineField();
+    public void shouldFlagWhenGridActionIsFlag() {
+        MineField minefield = new MineField("xx,xm");
+        Grid grid=new Grid(0,0,'F');
+        grid.setDisplayChar('F');
+        minefield.flagCell(0,0,'F');
+        assertEquals('F',grid.getDisplayChar());
     }
 }
