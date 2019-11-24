efefc1083638ce62b533324a04d6b1c16bad376d
sathyapriya <spriya2306@gmail.com>
2019-11-24 23:12:30 +0530

    [Sathya]Add.Test for Grid class

--- src/test/java/com/tw/vapasi/GridTest.java
+++ src/test/java/com/tw/vapasi/GridTest.java
@@ -6,26 +6,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GridTest {
     @Test
-    public void ShouldIsMineBeTrueIfItIsMineField(){
-        Grid grid=new Grid(0,0,'m');
-        assertEquals(true,grid.getIsMine());
+    public void ShouldIsMineBeTrueIfItIsMineField() {
+        Grid grid = new Grid(0, 0, 'm');
+        assertEquals(true, grid.getIsMine());
     }
 
     @Test
-    public void ShouldIsMineBeFalseWhenItIsNotMineField(){
-        Grid grid=new Grid(0,0,'x');
-        assertEquals(false,grid.getIsMine());
+    public void ShouldIsMineBeFalseWhenItIsNotMineField() {
+        Grid grid = new Grid(0, 0, 'x');
+        assertEquals(false, grid.getIsMine());
     }
 
     @Test
-    public void ShouldDisplayCharBeXWhenMineField(){
-        Grid grid=new Grid(0,0,'m');
-        assertEquals('X',grid.getDisplayChar());
+    public void ShouldDisplayCharBeXWhenMineField() {
+        Grid grid = new Grid(0, 0, 'm');
+        assertEquals('X', grid.getDisplayChar());
     }
 
     @Test
-    public void ShouldDisplayCharBeXWhenNotMineField(){
-        Grid grid=new Grid(0,0,'x');
-        assertEquals('X',grid.getDisplayChar());
+    public void ShouldDisplayCharBeXWhenNotMineField() {
+        Grid grid = new Grid(0, 0, 'x');
+        assertEquals('X', grid.getDisplayChar());
     }
 }
